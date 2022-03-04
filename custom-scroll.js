1313f34efa7796e29f13ea61c307cdd668495834
jrc03c <jrc03c@pm.me>
2022-03-04 13:14:11 -0600

    ...

--- custom-scroll.js
+++ custom-scroll.js
@@ -6,48 +6,91 @@ function curve(x) {
   return Math.sin(x * Math.PI - Math.PI / 2) * 0.5 + 0.5
 }
 
-function customScrollElementTo(element, x, y, ms) {
-  const xProperty = element === window ? "scrollX" : "scrollLeft"
-  const yProperty = element === window ? "scrollY" : "scrollTop"
-
+function pause(ms) {
   return new Promise((resolve, reject) => {
     try {
-      let isStillScrolling = true
-      let elapsedTime = 0
-      let lastTimestamp = new Date()
-
-      function loop() {
-        if (isStillScrolling) {
-          window.requestAnimationFrame(loop)
-        } else {
-          return resolve()
-        }
-
-        element.scrollTo(
-          lerp(element[xProperty], x, curve(elapsedTime / ms)),
-          lerp(element[yProperty], y, curve(elapsedTime / ms))
-        )
-
-        const currentTimestamp = new Date()
-        elapsedTime += currentTimestamp - lastTimestamp
-        lastTimestamp = currentTimestamp
-
-        if (elapsedTime >= ms) {
-          isStillScrolling = false
-        }
-      }
-
-      loop()
+      setTimeout(resolve, ms)
     } catch (e) {
       reject(e)
     }
   })
 }
 
+class CustomScroller {
+  constructor(element) {
+    const self = this
+    self.element = element
+    self.shouldKeepScrolling = false
+    self.hasStoppedScrolling = true
+  }
+
+  scrollTo(x, y, ms) {
+    const self = this
+    const xProperty = self.element === window ? "scrollX" : "scrollLeft"
+    const yProperty = self.element === window ? "scrollY" : "scrollTop"
+
+    return new Promise(async (resolve, reject) => {
+      try {
+        await self.stop()
+
+        self.shouldKeepScrolling = true
+        self.hasStoppedScrolling = false
+
+        let elapsedTime = 0
+        let lastTimestamp = new Date()
+
+        function loop() {
+          if (self.shouldKeepScrolling) {
+            window.requestAnimationFrame(loop)
+          } else {
+            self.hasStoppedScrolling = true
+            return resolve()
+          }
+
+          self.element.scrollTo(
+            lerp(self.element[xProperty], x, curve(elapsedTime / ms)),
+            lerp(self.element[yProperty], y, curve(elapsedTime / ms))
+          )
+
+          const currentTimestamp = new Date()
+          elapsedTime += currentTimestamp - lastTimestamp
+          lastTimestamp = currentTimestamp
+
+          if (elapsedTime >= ms) {
+            self.shouldKeepScrolling = false
+          }
+        }
+
+        loop()
+      } catch (e) {
+        reject(e)
+      }
+    })
+  }
+
+  stop() {
+    const self = this
+
+    return new Promise((resolve, reject) => {
+      try {
+        self.shouldKeepScrolling = false
+
+        let interval = setInterval(() => {
+          if (!self.hasStoppedScrolling) return
+          clearInterval(interval)
+          resolve()
+        }, 5)
+      } catch (e) {
+        reject(e)
+      }
+    })
+  }
+}
+
 if (typeof module !== "undefined") {
-  module.exports = customScrollElementTo
+  module.exports = CustomScroller
 }
 
 if (typeof window !== "undefined") {
-  window.customScrollElementTo = customScrollElementTo
+  window.customScrollElementTo = CustomScroller
 }
