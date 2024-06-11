55b36eda5e0729bd6fdfe8c82b648e44d40dbb33
jrc03c <jrc03c@pm.me>
2024-06-11 13:18:38 -0500

    ...

--- custom-scroller.js
+++ custom-scroller.js
@@ -2,60 +2,114 @@ function lerp(a, b, f) {
   return f * (b - a) + a
 }
 
-class CustomScroller {
-  constructor(element, easingFunction) {
-    const self = this
-    self.element = element
-    self.shouldKeepScrolling = false
-    self.hasStoppedScrolling = true
+function easeBounce(x) {
+  const f = x => Math.sin((x - 0.25) * Math.PI * 2) * 0.5 + 0.5
+  const g = x => Math.sin(((x - 0.5) * Math.PI * 2) / 1.265) * 0.8 + 0.5
+  const h = x => Math.sin(x * Math.PI - Math.PI / 2) * 0.5 + 0.5
+  const i = x => f(x) * g(x) + (1 - f(x)) * h(x)
+  return i(x)
+}
 
-    self.easingFunction =
-      easingFunction ||
-      function (x) {
-        return Math.sin(x * Math.PI - Math.PI / 2) * 0.5 + 0.5
-      }
+function easeIn(x) {
+  return -Math.sqrt(1 - x * x) + 1
+}
+
+function easeLinear(x) {
+  return x
+}
+
+function easeOut(x) {
+  return Math.sqrt(1 - Math.pow(x - 1, 2))
+}
+
+function easeSigmoid(x) {
+  return 1 / (1 + Math.exp(-16 * x + 8))
+}
+
+function easeSine(x) {
+  return Math.sin(x * Math.PI - Math.PI / 2) * 0.5 + 0.5
+}
+
+class CustomScroller {
+  static EASE_BOUNCE = easeBounce
+  static EASE_IN = easeIn
+  static EASE_LINEAR = easeLinear
+  static EASE_OUT = easeOut
+  static EASE_SIGMOID = easeSigmoid
+  static EASE_SINE = easeSine
+
+  constructor(options) {
+    options = options || {}
+    this.easingFunction = options.fn || CustomScroller.EASE_SINE
+    this.element = options.el || document.scrollingElement
+    this.hasStoppedScrolling = true
+    this.shouldKeepScrolling = false
   }
 
-  scrollTo(x, y, ms) {
-    const self = this
-    const xProperty = self.element === window ? "scrollX" : "scrollLeft"
-    const yProperty = self.element === window ? "scrollY" : "scrollTop"
-    const originalX = self.element[xProperty]
-    const originalY = self.element[yProperty]
+  scrollTo() {
+    const args = Array.from(arguments)
 
-    return new Promise(async (resolve, reject) => {
+    const el = args.find(
+      arg => arg instanceof HTMLElement || typeof arg === "string",
+    )
+
+    if (el) {
+      const ms = args.find(arg => typeof arg === "number") || 500
+      return this.scrollToElement(el, ms)
+    } else {
+      const x = args[0]
+      const y = args[1]
+      const ms = args[2] || 500
+      return this.scrollToPosition(x, y, ms)
+    }
+  }
+
+  scrollToElement(el, ms) {
+    el = typeof el === "string" ? document.querySelector(el) : el
+    const { x, y } = el.getBoundingClientRect()
+    return this.scrollToPosition(x, y, ms)
+  }
+
+  scrollToPosition(x, y, ms) {
+    const xProperty = this.element === window ? "scrollX" : "scrollLeft"
+    const yProperty = this.element === window ? "scrollY" : "scrollTop"
+    const originalX = this.element[xProperty]
+    const originalY = this.element[yProperty]
+
+    return new Promise((resolve, reject) => {
       try {
-        await self.stop()
+        this.stop().then(() => {
+          this.shouldKeepScrolling = true
+          this.hasStoppedScrolling = false
 
-        self.shouldKeepScrolling = true
-        self.hasStoppedScrolling = false
+          let elapsedTime = 0
+          let lastTimestamp = new Date()
 
-        let elapsedTime = 0
-        let lastTimestamp = new Date()
+          const loop = () => {
+            if (this.shouldKeepScrolling) {
+              window.requestAnimationFrame(loop)
+            } else {
+              this.hasStoppedScrolling = true
+              return resolve()
+            }
 
-        function loop() {
-          if (self.shouldKeepScrolling) {
-            window.requestAnimationFrame(loop)
-          } else {
-            self.hasStoppedScrolling = true
-            return resolve()
+            this.element.scrollTo(
+              lerp(originalX, x, this.easingFunction(elapsedTime / ms)),
+              lerp(originalY, y, this.easingFunction(elapsedTime / ms)),
+            )
+
+            const currentTimestamp = new Date()
+            elapsedTime += currentTimestamp - lastTimestamp
+            lastTimestamp = currentTimestamp
+
+            if (elapsedTime >= ms) {
+              this.shouldKeepScrolling = false
+              this.element.scrollTo(x, y)
+            }
           }
 
-          self.element.scrollTo(
-            lerp(originalX, x, self.easingFunction(elapsedTime / ms)),
-            lerp(originalY, y, self.easingFunction(elapsedTime / ms))
-          )
-
-          const currentTimestamp = new Date()
-          elapsedTime += currentTimestamp - lastTimestamp
-          lastTimestamp = currentTimestamp
-
-          if (elapsedTime >= ms) {
-            self.shouldKeepScrolling = false
-          }
-        }
-
-        loop()
+          loop()
+        })
       } catch (e) {
         reject(e)
       }
@@ -63,14 +117,12 @@ class CustomScroller {
   }
 
   stop() {
-    const self = this
-
     return new Promise((resolve, reject) => {
       try {
-        self.shouldKeepScrolling = false
+        this.shouldKeepScrolling = false
 
         let interval = setInterval(() => {
-          if (!self.hasStoppedScrolling) return
+          if (!this.hasStoppedScrolling) return
           clearInterval(interval)
           resolve()
         }, 5)
--- readme.md
+++ readme.md
@@ -10,13 +10,26 @@ npm install --save https://github.com/jrc03c/custom-scroller
 <script src="path/to/custom-scroller.js"></script>
 <script>
   const myScrollingElement = document.querySelector("#my-scrolling-element")
+  const linearEasing = x => x // optional; defaults to sine easing
+
+  const scroller = new CustomScroller({
+    el: myScrollingElement,
+    fn: linearEasing,
+  })
+
+  // scroll to a position:
   const x = 50
   const y = 250
   const ms = 1500
-  const linearEasing = x => x // optional; defaults to sine easing
-  const scroller = new CustomScroller(myScrollingElement, linearEasing)
 
   scroller.scrollTo(x, y, ms).then(() => {
+    // do something cool
+  })
+
+  // scroll to an element:
+  const el = document.querySelector(".my-cool-element")
+
+  scroller.scrollTo(el, ms).then(() => {
     // do something cool
   })
 </script>
